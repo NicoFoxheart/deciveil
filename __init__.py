0d0670328da203c344ae2271d5a3bf7befa1d7f0
Nicolas Graham <115115867+NicoFoxheart@users.noreply.github.com>
2026-07-05 16:06:34 -0400

    Delete __init__.py

--- __init__.py
+++
@@ -1,6 +0,0 @@
-# Deciveil modules
-from .scam_detector import analyze_scam
-from .lie_detector import analyze_manipulation
-from .deepfake_detector import analyze_image
-
-__all__ = ["analyze_scam", "analyze_manipulation", "analyze_image"]
