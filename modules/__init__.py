bbaaef3fb5ad5ba7df5255d2374ae59fee41199e
Nicolas Graham <115115867+NicoFoxheart@users.noreply.github.com>
2026-07-05 16:11:12 -0400

    Initialize module with analysis functions

---
+++ modules/__init__.py
@@ -0,0 +1,5 @@
+from .scam_detector import analyze_scam
+from .lie_detector import analyze_manipulation
+from .deepfake_detector import analyze_image
+
+__all__ = ["analyze_scam", "analyze_manipulation", "analyze_image"]
