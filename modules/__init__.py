e965bba1a3bd04a7a6ec419de71e98c443889287
Nicolas Graham <115115867+NicoFoxheart@users.noreply.github.com>
2026-07-05 23:57:55 -0400

    Update __init__.py

--- modules/__init__.py
+++ modules/__init__.py
@@ -1,5 +1,15 @@
 from .scam_detector import analyze_scam
 from .lie_detector import analyze_manipulation
-from .deepfake_detector import analyze_image
+from .url_scanner import analyze_url
+from .password_analyzer import analyze_password
+from .hash_identifier import identify_hash
+from .encoding_detector import analyze_encoding
 
-__all__ = ["analyze_scam", "analyze_manipulation", "analyze_image"]
+__all__ = [
+    "analyze_scam",
+    "analyze_manipulation",
+    "analyze_url",
+    "analyze_password",
+    "identify_hash",
+    "analyze_encoding",
+]
